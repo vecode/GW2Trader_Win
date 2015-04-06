1378c8deb07679c2bb8b0ac58bc56e25688db3bf
Marvin Danker <marvindanker@gmail.com>
2015-04-06 22:18:44 +0200

    shared added

---
+++ Shared/Exception/ApiNotAccessibleException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Exception
+{
+    public class ApiNotAccessibleException : System.Exception
+    {
+        private string _apiUrl;
+
+        public ApiNotAccessibleException(string ApiUrl)
+        {
+            _apiUrl = ApiUrl;
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return String.Format("Api endpoint {0} is not accessible.", _apiUrl);
+            }
+        }
+    }
+}
