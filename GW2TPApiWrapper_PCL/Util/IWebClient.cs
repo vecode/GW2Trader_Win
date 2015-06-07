551a1fe69e6ec98ece1215b6d6903da47d77fcaa
vecode <marvindanker@gmail.com>
2015-06-07 05:18:28 +0200

    Add missing file

---
+++ GW2TPApiWrapper_PCL/Util/IWebClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GW2TPApiWrapper.Util
+{
+    public interface IWebClient : IDisposable
+    {
+        Stream OpenRead(string url);
+    }
+}
