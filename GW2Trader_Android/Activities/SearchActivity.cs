0ae81e3d7afa559406e48e9696335e2125e1ecc6
vecode <marvindanker@gmail.com>
2015-06-09 21:14:19 +0200

    Improve search UI

--- GW2Trader_Android/Activities/SearchActivity.cs
+++ GW2Trader_Android/Activities/SearchActivity.cs
@@ -13,14 +13,13 @@ using GW2Trader.Manager;
 using TinyIoC;
 using GW2Trader.Model;
 using GW2Trader_Android.Adapter;
+using GW2Trader_Android.Filter;
 
 namespace GW2Trader_Android.Activities
 {
     [Activity(Label = "SearchActivity")]
     public class SearchActivity : Activity
     {
-        private RelativeLayout _filterLayout;
-        private Button _filterToggleButton;
         private Button _searchButton;
         private SearchView _searchView;
         private ListView _resultListView;
@@ -30,21 +29,38 @@ namespace GW2Trader_Android.Activities
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+            InitUI();
+
+
+            _itemManager = TinyIoCContainer.Current.Resolve<IItemManager>();
+        }
+
+        private void InitUI()
+        {
             SetContentView(Resource.Layout.Search);
 
             _searchButton = FindViewById<Button>(Resource.Id.ExecuteSearchButton);
             _searchButton.Click += OnSearchButtonClicked;
 
-            _filterToggleButton = FindViewById<Button>(Resource.Id.FilterToggleButton);
-            _filterToggleButton.Click += OnFilterToggleClicked;
-
-            _filterLayout = FindViewById<RelativeLayout>(Resource.Id.FilterLayout);
-            _filterLayout.Visibility = ViewStates.Gone;
-
             _searchView = FindViewById<SearchView>(Resource.Id.SearchView);
-            _resultListView = FindViewById<ListView>(Resource.Id.SearchResultListView);
 
-            _itemManager = TinyIoCContainer.Current.Resolve<IItemManager>();
+            EditText minLvl = FindViewById<EditText>(Resource.Id.MinLevel);
+            minLvl.SetFilters(new Android.Text.IInputFilter[] { new RangeInputFilter(0, 80) });
+
+            EditText maxLvl = FindViewById<EditText>(Resource.Id.MaxLevel);
+            maxLvl.SetFilters(new Android.Text.IInputFilter[] { new RangeInputFilter(0, 80) });
+
+            Spinner raritySpinner = FindViewById<Spinner>(Resource.Id.RaritySpinner);
+            var rarityAdapter = ArrayAdapter.CreateFromResource(
+                this, Resource.Array.raritiy_array, Android.Resource.Layout.SimpleSpinnerItem);
+            rarityAdapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            raritySpinner.Adapter = rarityAdapter;
+
+            Spinner typeSpinner = FindViewById<Spinner>(Resource.Id.TypeSpinner);
+            var typeAdapter = ArrayAdapter.CreateFromResource(
+                this, Resource.Array.type_array, Android.Resource.Layout.SimpleSpinnerItem);
+            typeAdapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            typeSpinner.Adapter = typeAdapter;
         }
 
         private void OnSearchButtonClicked(object sender, EventArgs e)
@@ -59,22 +75,8 @@ namespace GW2Trader_Android.Activities
 
             //var items = new string[] {query, query+"2", query+"3"};
 
-            var adapter = new ItemsAdapter(this, items);
-            _resultListView.Adapter = adapter;
-        }
-
-        private void OnFilterToggleClicked(object sender, EventArgs e)
-        {
-            if (_filterLayout.Visibility == ViewStates.Gone)
-            {
-                _filterLayout.Visibility = ViewStates.Visible;
-                _filterToggleButton.Text = Resources.GetString(Resource.String.HideFilter);
-            }
-            else
-            {
-                _filterLayout.Visibility = ViewStates.Gone;
-                _filterToggleButton.Text = Resources.GetString(Resource.String.ExpandFilter);
-            }
+            //var adapter = new ItemsAdapter(this, items);
+            //_resultListView.Adapter = adapter;
         }
     }
 }
--- GW2Trader_Android/Resources/Resource.Designer.cs
+++ GW2Trader_Android/Resources/Resource.Designer.cs
@@ -30,6 +30,25 @@ namespace GW2Trader_Android
 			global::SQLite.Net.Platform.XamarinAndroid.Resource.String.Hello = global::GW2Trader_Android.Resource.String.Hello;
 		}
 		
+		public partial class Array
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int raritiy_array = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int type_array = 2130968577;
+			
+			static Array()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Array()
+			{
+			}
+		}
+		
 		public partial class Attribute
 		{
 			
@@ -65,59 +84,77 @@ namespace GW2Trader_Android
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f05000b
-			public const int ExecuteSearchButton = 2131034123;
+			// aapt resource value: 0x7f060012
+			public const int ExecuteSearchButton = 2131099666;
 			
-			// aapt resource value: 0x7f050008
-			public const int FilterLayout = 2131034120;
+			// aapt resource value: 0x7f060016
+			public const int Icon = 2131099670;
 			
-			// aapt resource value: 0x7f050007
-			public const int FilterToggleButton = 2131034119;
+			// aapt resource value: 0x7f060002
+			public const int InvestmentButton = 2131099650;
 			
-			// aapt resource value: 0x7f050010
-			public const int Icon = 2131034128;
+			// aapt resource value: 0x7f060014
+			public const int ItemName = 2131099668;
 			
-			// aapt resource value: 0x7f050002
-			public const int InvestmentButton = 2131034114;
+			// aapt resource value: 0x7f060009
+			public const int LevelTextView = 2131099657;
 			
-			// aapt resource value: 0x7f05000e
-			public const int ItemName = 2131034126;
+			// aapt resource value: 0x7f06000d
+			public const int MaxLevel = 2131099661;
 			
-			// aapt resource value: 0x7f05000f
-			public const int Rarity = 2131034127;
+			// aapt resource value: 0x7f06000c
+			public const int MaxLevelTextView = 2131099660;
 			
-			// aapt resource value: 0x7f050000
-			public const int SearchButton = 2131034112;
+			// aapt resource value: 0x7f06000b
+			public const int MinLevel = 2131099659;
 			
-			// aapt resource value: 0x7f050006
-			public const int SearchFilterText = 2131034118;
+			// aapt resource value: 0x7f06000a
+			public const int MinLevelTextView = 2131099658;
 			
-			// aapt resource value: 0x7f05000c
-			public const int SearchResultListView = 2131034124;
+			// aapt resource value: 0x7f060015
+			public const int Rarity = 2131099669;
 			
-			// aapt resource value: 0x7f050004
-			public const int SearchView = 2131034116;
+			// aapt resource value: 0x7f06000f
+			public const int RaritySpinner = 2131099663;
 			
-			// aapt resource value: 0x7f050003
-			public const int SettingsButton = 2131034115;
+			// aapt resource value: 0x7f060000
+			public const int SearchButton = 2131099648;
 			
-			// aapt resource value: 0x7f05000d
-			public const int Text = 2131034125;
+			// aapt resource value: 0x7f060007
+			public const int SearchView = 2131099655;
 			
-			// aapt resource value: 0x7f050011
-			public const int UpdateDbButton = 2131034129;
+			// aapt resource value: 0x7f060003
+			public const int SettingsButton = 2131099651;
 			
-			// aapt resource value: 0x7f050001
-			public const int WatchlistButton = 2131034113;
+			// aapt resource value: 0x7f060013
+			public const int Text = 2131099667;
 			
-			// aapt resource value: 0x7f050009
-			public const int button1 = 2131034121;
+			// aapt resource value: 0x7f060011
+			public const int TypeSpinner = 2131099665;
 			
-			// aapt resource value: 0x7f050005
-			public const int linearLayout1 = 2131034117;
+			// aapt resource value: 0x7f060017
+			public const int UpdateDbButton = 2131099671;
 			
-			// aapt resource value: 0x7f05000a
-			public const int space1 = 2131034122;
+			// aapt resource value: 0x7f060001
+			public const int WatchlistButton = 2131099649;
+			
+			// aapt resource value: 0x7f060006
+			public const int linearLayout1 = 2131099654;
+			
+			// aapt resource value: 0x7f060008
+			public const int linearLayout2 = 2131099656;
+			
+			// aapt resource value: 0x7f06000e
+			public const int linearLayout3 = 2131099662;
+			
+			// aapt resource value: 0x7f060010
+			public const int linearLayout4 = 2131099664;
+			
+			// aapt resource value: 0x7f060004
+			public const int relativeLayout1 = 2131099652;
+			
+			// aapt resource value: 0x7f060005
+			public const int scrollView1 = 2131099653;
 			
 			static Id()
 			{
@@ -157,35 +194,35 @@ namespace GW2Trader_Android
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040004
-			public const int ApplicationName = 2130968580;
+			// aapt resource value: 0x7f050004
+			public const int ApplicationName = 2131034116;
 			
-			// aapt resource value: 0x7f040007
-			public const int ExpandFilter = 2130968583;
+			// aapt resource value: 0x7f050007
+			public const int ExpandFilter = 2131034119;
 			
-			// aapt resource value: 0x7f040009
-			public const int Hello = 2130968585;
+			// aapt resource value: 0x7f050009
+			public const int Hello = 2131034121;
 			
-			// aapt resource value: 0x7f040008
-			public const int HideFilter = 2130968584;
+			// aapt resource value: 0x7f050008
+			public const int HideFilter = 2131034120;
 			
-			// aapt resource value: 0x7f040002
-			public const int Investments = 2130968578;
+			// aapt resource value: 0x7f050002
+			public const int Investments = 2131034114;
 			
-			// aapt resource value: 0x7f040000
-			public const int Search = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int Search = 2131034112;
 			
-			// aapt resource value: 0x7f040006
-			public const int SearchFilterText = 2130968582;
+			// aapt resource value: 0x7f050006
+			public const int SearchFilterText = 2131034118;
 			
-			// aapt resource value: 0x7f040003
-			public const int Settings = 2130968579;
+			// aapt resource value: 0x7f050003
+			public const int Settings = 2131034115;
 			
-			// aapt resource value: 0x7f040005
-			public const int UpdateDatabase = 2130968581;
+			// aapt resource value: 0x7f050005
+			public const int UpdateDatabase = 2131034117;
 			
-			// aapt resource value: 0x7f040001
-			public const int Watchlists = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int Watchlists = 2131034113;
 			
 			static String()
 			{
