5d3d81ed6ac179dc75b6e281ea93d749406b20ce
vecode <marvindanker@gmail.com>
2015-06-16 22:46:21 +0200

    Add MoneyHelper + unit tests

---
+++ GW2TraderTest/MoneyHelperTest.cs
@@ -0,0 +1,31 @@
+﻿using GW2Trader.Util;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace GW2TraderTest
+{
+    public class MoneyHelperTest
+    {
+        [Fact]
+        public void ExtractCopperTest()
+        {
+            Assert.Equal(56, MoneyHelper.ExtractPositivCopperShare(123456));
+            Assert.Equal(56, MoneyHelper.ExtractPositivCopperShare(-123456));
+        }
+
+        [Fact]
+        public void ExtractSilverTest()
+        {
+            Assert.Equal(34, MoneyHelper.ExtractPositivSilverShare(123456));
+            Assert.Equal(34, MoneyHelper.ExtractPositivSilverShare(-123456));
+        }
+
+        [Fact]
+        public void ExtractGoldTest()
+        {
+            Assert.Equal(123, MoneyHelper.ExtractPositivGoldShare(1234567));
+            Assert.Equal(123, MoneyHelper.ExtractPositivGoldShare(-1234567));
+        }
+    }
+}
